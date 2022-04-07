ebdafe416bc88ce862e7551a5789e2c250a7b282
Hana <hana.degefa2010@gmail.com>
2022-04-07 16:39:02 -0700

    sizetype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of a long int: %d byte(s) \n", sizeof(y));
 	printf("Size of a long long int: %d byte(s) \n", sizeof(z));
 	printf("Size of a float: %d byte(s) \n", sizeof(a));
-	return(0);
+	return (0);
 }
 
