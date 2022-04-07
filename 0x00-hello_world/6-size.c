fcdaf34bcc82a24d633cdffedd1f825c23063b24
Hana <hana.degefa2010@gmail.com>
2022-04-07 16:41:14 -0700

    sizetype

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float a;
 
 	printf("Size of a char: %d byte(s) \n", sizeof(x));
-	printf("Size of an int: %d byte(s) \n", sizeof (i));
+	printf("Size of an int: %d byte(s) \n", sizeof(i));
 	printf("Size of a long int: %d byte(s) \n", sizeof(y));
 	printf("Size of a long long int: %d byte(s) \n", sizeof(z));
 	printf("Size of a float: %d byte(s) \n", sizeof(a));
