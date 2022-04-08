a79ec286b634c474dcb3cc50b95eef0755c39ed1
Hana <hana.degefa2010@gmail.com>
2022-04-08 14:33:15 -0700

    letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * main - print letters except q and e
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
