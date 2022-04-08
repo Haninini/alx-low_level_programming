93487a209ed90ad66f09bbf4bd828178d4469200
Hana <hana.degefa2010@gmail.com>
2022-04-08 15:53:47 -0700

    numberscomb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/** 
+ * main - print numbers combination
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar('0' + x);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
