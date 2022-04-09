cbcac881895e8e3e3f4c35161854f8080040d1e4
Hana <hana.degefa2010@gmail.com>
2022-04-08 17:36:41 -0700

    three digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int x, y;
+
 	for (x = 0; x <= 98; x++)
 	{
 		for (y = x + 1; y <= 99; y++)
@@ -19,7 +20,7 @@ int main(void)
 			putchar((y / 10) + '0');
 			putchar((y % 10) + '0');
 
-		if ( x == 98 && y == 99)
+		if (x == 98 && y == 99)
 			continue;
 		putchar(',');
 		putchar(' ');
@@ -27,5 +28,5 @@ int main(void)
 	}
 
 	putchar('\n');
-	return(0);
+	return (0);
 }
