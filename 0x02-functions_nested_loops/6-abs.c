ddde4ad0665a7e0600b3c8951fe97d11b1a3ea3f
Hana <hana.degefa2010@gmail.com>
2022-04-12 14:10:36 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an integer
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	if (int < 0)
+	{
+		int = (-1) * int;
+			_putchar(int + '0');
+	}
+			return (0);
+}
