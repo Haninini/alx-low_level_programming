f497c75315b5dce42961115ce6a1478817dc10fb
Hana <hana.degefa2010@gmail.com>
2022-04-12 13:11:25 -0700

    try

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ *
+ * @n - the number tested for the sign
+ * Return: 0 if the number is zero, 1 if the number is positive and -1 if the number is negative
+ */
+
+int print_sign(int n)
+{
+
