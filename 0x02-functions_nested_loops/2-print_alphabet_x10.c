cf85ca0bafd5106b4091acb9fe32138a4e6dd191
Hana <hana.degefa2010@gmail.com>
2022-04-12 10:36:57 -0700

    ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - 
+ *
+ * desciption - prints the alphabet ten times
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
