efd0c2fdcefb79e8f6cbbd2163bb321c243453d9
Hana <hana.degefa2010@gmail.com>
2022-04-12 10:18:37 -0700

    alphapbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * main - print alphabet function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+}
