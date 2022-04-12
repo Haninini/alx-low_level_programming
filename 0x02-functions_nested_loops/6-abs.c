9274d00d524733e07d5f92a173c20bf7219bab25
Hana <hana.degefa2010@gmail.com>
2022-04-12 14:22:14 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,16 +4,13 @@
  * _abs - compute the absolute value of an integer
  *
  * @x : number to be computed
- * Return: 0
+ * Return: the value
  */
 
 int _abs(int x)
 {
 	if (x < 0)
-	{
 		x = (-1) * x;
-	_putchar(x + '0');
-	}
 
-			return (0);
+	return (x);
 }
