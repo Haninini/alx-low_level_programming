2a5ee076367781ca28054f56862a84097fc821e6
Hana <hana.degefa2010@gmail.com>
2022-04-12 17:08:09 -0700

    print

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
  * print_to_98 - prints all natural numbers from n to ninety eight
  *
- * @n - number given
+ * @n - number given to start from printing
  */
 
 void print_to_98(int n)
@@ -14,19 +14,15 @@ void print_to_98(int n)
 	{
 		for (x = n; x <= 98; x++)
 		{
-			_putchar(x + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, " x);
 		}
 	}
 	else if (n > 98)
 	{
 		for (x = n; x >= 98; x--)
 		{
-			_putchar(x + '0');
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, " x);
 		}
 	}
-	_putchar('\n');
+	printf("\n");
 }
