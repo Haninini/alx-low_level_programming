edb7f321b26c9db0679bf7d19c1eef3b63a05f13
Hana <hana.degefa2010@gmail.com>
2022-04-15 03:26:43 -0700

    select

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the number 0 to 9
+ * print_most_numbers - print the number except 2 and 4
  *
  */
 
@@ -14,7 +14,7 @@ void print_most_numbers(void)
 		if (x == 2 || x == 4)
 			x++;
 
-		_putchar(i + '0');
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
