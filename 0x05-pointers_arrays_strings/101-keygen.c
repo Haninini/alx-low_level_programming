19054c1266d02341ccf6e74d9220d0479c36c2c6
Hana <hana.degefa2010@gmail.com>
2022-04-18 16:51:24 -0700

    pswd

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,11 +10,16 @@
 int main(void)
 {
 	int x;
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
 
 	srand((unsigned int)(time(NULL)));
 	x = rand();
+	for (int i = 0; i < 5; i++)
+	{
+	  printf("%c", list[rand() % (sizeof list - 1)]);
+	}
 	
-	printf("%i\n", x);
+	/*printf("%i\n", x);*/
 
 	return (0);
 }
