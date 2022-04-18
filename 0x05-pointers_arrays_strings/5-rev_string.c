a295fdb1fa45520908778e8dbec1190b5564f2f0
Hana <hana.degefa2010@gmail.com>
2022-04-18 13:31:55 -0700

    reversestr

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: the string given as parameter
+ *
+ */
+
+void rev_string(char *s)
+{
+	char revs[10];
+	char *revp = revs;
+	int i = -1;
+
+	while (*s)
+	{
+		s++;
+		i++;
+	}
+	while (i >= 0)
+	{
+		s--;
+		*revp = *s;
+		*revp++;
+		--i;
+	}
+	*revp = '\0';
+	*s = *revp;
+}
+
