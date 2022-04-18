f689d24b6049db10c3584af7b8b03cd9b3587875
Hana <hana.degefa2010@gmail.com>
2022-04-18 14:47:36 -0700

    jump

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i+=2)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		printf("%c", *(str + i));
+		if (i % 2 == 0)
+         	printf("%c", *(str + i));
 	}
 
 	printf("\n");
 }
-
