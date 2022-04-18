a3ff72a2a50600c40c4ed74a6990bcc4eb7a0b4a
Hana <hana.degefa2010@gmail.com>
2022-04-18 14:34:54 -0700

    jump

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the given string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i+=2)
+	{
+		printf("%c", *(str + i));
+	}
+
+	printf('\n');
+}
+
