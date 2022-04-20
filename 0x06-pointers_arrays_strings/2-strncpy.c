c555fb7a1d617313af83eab258a627fc6faccb7c
Hana <hana.degefa2010@gmail.com>
2022-04-20 08:50:37 -0700

    copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/** _strncpy - copies a string
+ * @dest: destination where the content is to be copied
+ * @src: the source string which will be copied
+ * @n: the first n char to be copied from src to dest
+ *
+ * Return: the sestination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *s1 = dest;
+
+	while (*src && n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (s1);
+}
+
