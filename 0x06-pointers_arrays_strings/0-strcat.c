471f33dedf17b178e208a7214919746f4277b8ed
Hana <hana.degefa2010@gmail.com>
2022-04-21 10:14:26 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,13 +13,12 @@ char *_strcat(char *dest, char *src)
 
 	int x, y;
 
-	for (x = 0; dest[x] != '\0'; x++)
+	for (x = 0; dest[x] != '\0'; x++);
+
+	for (y = 0; src[y] != '\0'; y++)
 	{
-		for (y = 0; src[y] != '\0'; y++)
-		{
-			dest[x] = src [y];
-			x++;
-		}
+		dest[x] = src [y];
+		x++;
 	}
 
 	*dest = '\0';
