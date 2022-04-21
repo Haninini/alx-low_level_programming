c71f0ccc167ed356bb3dfdf8f0a85d7545c94d2d
Hana <hana.degefa2010@gmail.com>
2022-04-21 14:55:38 -0700

    strcom

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int status;
+
 	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
 		s2++;
 	}
-	if (*s1 === *s2)
-		return (0);
-	else if (*s1 < *s2)
-		return (-1);
-	else
-		return (1);
+	status = (*s1 == *s2)?0 : (*s1 > *s2)?1 : -1;
+	return (status);
 }
