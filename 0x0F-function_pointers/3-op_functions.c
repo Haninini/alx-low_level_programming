cde2facd06699280e062952c67080b5eb62bfeb5
Haninini <30465771+Haninini@users.noreply.github.com>
2022-05-12 04:33:25 +0300

    operation

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,23 +7,23 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - returns the sum of two numbers
- * @a: the first number
- * @b: the second number
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: the sum of a and b
+ * Return: The sum of a and b.
  */
-
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - returns the substraction of two numbers
- * @a: the first number
- * @b: the second number
- * Return: the substraction of a and b
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
  */
 int op_sub(int a, int b)
 {
@@ -31,10 +31,11 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - returns the product of two numbers
- * @a: the first number
- * @b: the second number
- * Return: the multplication of a and b
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
@@ -42,10 +43,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - returns the division of two numbers
- * @a: the first number
- * @b: the second number
- * Return: the quotient of a and b
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
 {
@@ -53,13 +55,13 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - return the remainder of the division of two numbers
- * @a:  the first number
- * @b: the second number
- * Return: the remainder of the division of a by b
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
 	return (a % b);
 }
-
