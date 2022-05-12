707d6a8ea7a285e003a767f054d22a4386088953
Hana <hana.degefa2010@gmail.com>
2022-05-12 07:18:50 -0700

    sum all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
 #include <stdarg.h>
 
 /**
@@ -14,7 +15,7 @@ typedef struct printer
 
 }printer_t;
 
-int _putchar( char a);
+int _putchar(char a);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
