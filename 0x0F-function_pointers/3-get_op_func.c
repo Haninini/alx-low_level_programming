54179dbc504ed3f9b957a1c89c77393d005d3e7f
Haninini <30465771+Haninini@users.noreply.github.com>
2022-05-12 04:30:44 +0300

    operation function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 
 /**
- * get_op_func - select the correct function to perform the operation asked by the user
- * @s: the operator passed as argument
- * Return: a pointer to the function corresponding to the operator given as a parameter
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
+ *
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -16,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i =0;
+	int i = 0;
 
 	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
