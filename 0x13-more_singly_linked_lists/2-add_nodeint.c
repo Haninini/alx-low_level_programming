a1002b1710dcdf9158770f8996c78a564565716e
Haninini <30465771+Haninini@users.noreply.github.com>
2022-05-25 22:01:49 +0300

    new node at the begining

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
-#include <studio.h>
 
 /**
- * add_nodeint - adds a new node at the beginning of a listint_t list
- * @head: head of a list
- * @n: n element
- * Return: address of the new element. Null if it failed
+ * add_nodeint - add a new node at the beginning
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
