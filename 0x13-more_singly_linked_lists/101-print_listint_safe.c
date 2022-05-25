18335141d33fa6ee50991affa3f9cb6f5a75e936
Haninini <30465771+Haninini@users.noreply.github.com>
2022-05-26 01:55:59 +0300

    print list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,78 +1,60 @@
 #include "lists.h"
 #include <stdio.h>
 
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
-
 /**
- * print_listint_safe - counts the number of unique nodes
- * @head: a pointer to the head of the list to check
- * Return: if the list is not looped - 0, otherwise the number
- *         of unique nodes in the list
+ * _r - reallocates memory for an array of pointers
+ * to the nodes in a linked list
+ * @list: the old list to append
+ * @size: size of the new list (always one more than the old list)
+ * @new: new node to add to the list
+ *
+ * Return: pointer to the new list
  */
-size_t print_listint_safe(const listint_t *head)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
-	const listint_t *tort, *h;
-	size_t nodes = 1;
+	const listint_t **newlist;
+	size_t i;
 
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tort = head->next;
-	h = (head->next)->next;
-
-	while (h)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
 	{
-		if (tort == h)
-		{
-			tort = head;
-			while (tort != h)
-			{
-				nodes++;
-				tort = tort->next;
-				h = h->next;
-			}
-			tort = tort->next;
-			while (tort != h)
-			{
-				nodes++;
-				tort = tort->next;
-			}
-			return (nodes);
-		}
-		tort = tort->next;
-		h = (h->next)->next;
+		free(list);
+		exit(98);
 	}
-	return (0);
+	for (i = 0; i < size - 1; i++)
+		newlist[i] = list[i];
+	newlist[i] = new;
+	free(list);
+	return (newlist);
 }
 
 /**
- * print_listint_safe - prints a listint_t safely
- * @head: a pointer to the head
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the start of the list
+ *
  * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes, indx = 0;
+	size_t i, num = 0;
+	const listint_t **list = NULL;
 
-	nodes = looped_listint_len(head);
-
-	if (nodes == 0)
+	while (head != NULL)
 	{
-		for (; head != NULL; nodes++)
+		for (i = 0; i < num; i++)
 		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
+			if (head == list[i])
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free(list);
+				return (num);
+			}
 		}
+		num++;
+		list = _r(list, num, head);
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 	}
-	else
-	{
-		for (indx = 0; indx < nodes; indx++)
-		{
-			printf("[%p] %d\n", (void *)head, head->n);
-			head = head->next;
-		}
-		printf("-> [%p] %d\n", (void *)head, head->n);
-	}
-	return (nodes);
+	free(list);
+	return (num);
 }
