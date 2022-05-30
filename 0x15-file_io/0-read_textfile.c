1837a4e530647b3ded2243b2243ab5c9c777a91b
Haninini <30465771+Haninini@users.noreply.github.com>
2022-05-30 23:23:43 +0300

    read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcnt1.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
