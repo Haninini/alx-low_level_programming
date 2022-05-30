14cea808d6a97cfe5a67f835f103ad6f630293b2
Haninini <30465771+Haninini@users.noreply.github.com>
2022-05-30 23:06:29 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, len = 0;
+		int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -20,13 +20,13 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(op, text_content, len);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
-	if (op == -1 || wr == -1)
+	if (o == -1 || w == -1)
 		return (-1);
 
-	close(op);
+	close(o);
 
 	return (1);
 }
