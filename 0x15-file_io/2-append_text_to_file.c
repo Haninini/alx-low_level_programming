7dce603d6e2138b7257117b9423aebf0e879e8c1
Haninini <30465771+Haninini@users.noreply.github.com>
2022-05-30 23:13:24 +0300

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int opn, wrt, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	opn = open(filename, O_WRONLY | O_APPEND);
+	wrt = write(opn, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (opn == -1 || wrt == -1)
 		return (-1);
 
-	close(o);
+	close(opn);
 
 	return (1);
 }
